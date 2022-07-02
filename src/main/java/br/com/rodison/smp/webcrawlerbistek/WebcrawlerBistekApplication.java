af938f3914dd83feb15ba31c084523f620f8e242
rodisonferreira <rodison.ferreira@jexperts.com.br>
2022-07-01 22:36:22 -0300

    initial commit

---
+++ src/main/java/br/com/rodison/smp/webcrawlerbistek/WebcrawlerBistekApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodison.smp.webcrawlerbistek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcrawlerBistekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcrawlerBistekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodison/smp/webcrawlerbistek/WebcrawlerBistekApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodison.smp.webcrawlerbistek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcrawlerBistekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
